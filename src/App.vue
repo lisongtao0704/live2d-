12f40c91dcc73390c18a19c630f597454cdb7c87
list <3210677428@qq.com>
2025-03-06 10:37:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import { Live2DCubismFramework as live2dcubismframework } from '@framework/live2dcubismframework.ts';
+import CubismFramework = live2dcubismframework.CubismFramework;
+console.log(888,CubismFramework )
+// CubismFramework.startup();
+</script>
+
+<template>
+  <div>1111</div>
+</template>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, UserConfig, ConfigEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig((env: ConfigEnv): UserConfig => {
+
+  let common:defineConfig = {
+    plugins: [vue()],
+    server: {
+      port: 5000,
+    },
+    root: './',
+    base: '/',
+    publicDir: './public',
+    resolve: {
+      extensions: ['.ts', '.js']  ,
+      alias: {
+        '@framework': path.resolve(__dirname, './src/Framework/src'),
+      }
+    },
+    build: {
+      target: 'modules',
+      assetsDir: 'assets',
+      outDir: './dist',
+      sourcemap: env.mode == 'development' ? true : false,
+    },
+  }
+
+  return common
+})
